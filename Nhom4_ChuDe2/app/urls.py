913e474d77b505a88184fcb93d2305bffdedfa27
Thu_Thuy <maithithuthuy05@gmail.com>
2026-03-27 23:59:11 +0700

    Update urls.py

--- Nhom4_ChuDe2/app/urls.py
+++ Nhom4_ChuDe2/app/urls.py
@@ -8,14 +8,6 @@ urlpatterns = [
     path('logout/', views_khach.logout_view, name='logout'),
     path('success/', views_khach.success_view, name='success'),
 
-    # --- LUỒNG KHÁCH HÀNG (CUSTOMER) ---
-    # path('', views_khach.home, name='home'),
-    # path('register/khach/', views_khach.register_khach, name='register_khach'),
-    # path('shop/<int:id>/', views_khach.shop_detail, name='shop_detail'),
-    # path('tao-yeu-cau/', views_khach.create_request, name='create_request'),
-    # path('xem-bao-gia/', views_khach.confirm_quote, name='confirm_quote'),
-    # path('thanh-toan/<int:order_id>/', views_khach.payment, name='payment'),
-    # path('theo-doi-don-hang/', views_khach.order_status, name='order_status'),
     # --- LUỒNG KHÁCH HÀNG ---
     path('', views_khach.home_view, name='home'),
     path('after-login/', views_khach.after_login_view, name='after_login'),
@@ -57,6 +49,8 @@ urlpatterns = [
     path('vendor/gui-bao-gia/<int:req_id>/', views_tiem.send_quote, name='send_quote'),
 
     # --- LUỒNG QUẢN TRỊ (ADMIN) ---
+    path('admin-hub/login/', views_admin.login_admin, name='login_admin'),
+    path('admin-hub/logout/', views_admin.logout_admin, name='logout_admin'),
     path('admin-hub/dashboard/', views_admin.admin_dashboard, name='admin_dashboard'),
     path('admin-hub/users/', views_admin.user_management, name='admin_users'),
 ]
